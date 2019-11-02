166d6bcf4a1744df4521bbddb3a2a3736ba5cc74
Douglas Ciarfella <losdougie@comcast.net>
2019-11-02 19:09:22 -0400

    Added models and admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ToDo, ToDoType
+
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'priority', 'status', 'title','working')
+    list_display_links = ('id', 'title')
+    list_filter = ('status',)
+    list_editable = ('working',)
+    search_fields = ('title', 'desc')
+    list_per_page = 20
+
+admin.site.register(ToDo, ToDoAdmin)
+admin.site.register(ToDoType)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class ToDoType(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.type
+
+
+class ToDo(models.Model):
+    id = models.AutoField(primary_key=True)
+    priority = models.IntegerField(default=0)
+    title = models.CharField(max_length=255)
+    desc = models.TextField(blank=True)
+    type = models.ForeignKey(ToDoType, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=255)
+    # associated_files = models.FileField(upload_to='files/')
+    working = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
