e9a25135128070c9589d5db91e0f72efdb6175a8
Douglas Ciarfella <losdougie@comcast.net>
2019-11-02 22:36:41 -0400

    Added pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('about', views.about, name="about")
+    path('about', views.about, name="about"),
+    path('todo', views.todo, name="todo"),
+    path('time', views.show_time, name="show_time"),
+    path('todo_item/<int:todo_id>', views.todo_item, name="todo_item")
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import datetime_safe
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from .models import ToDo
 
 # Create your views here.
 def index(request):
@@ -38,6 +42,24 @@ def index(request):
 # templates are set in <project> settings.py TEMPLATES > DIRS list
 # add the new route to the urls
 
+def todo(request):
+    #add to pages/urls.py to add a new page
+    #all_todos = ToDo.objects.all()
+    all_todos = ToDo.objects.order_by('-id') # reverse listing by id
+
+    paginator = Paginator(all_todos, 20)
+    page = request.GET.get('page')
+    paged_todos = paginator.get_page(page)
+
+    todo_items = {"todos": paged_todos}
+    return render(request, 'pages/todo.html', todo_items)
+
+def todo_item(request, todo_id):
+    return render(request, 'pages/todo_item.html', {"id": todo_id})
+
 def about(request):
     # requires render
-    return render(request, 'pages/about.html')
+    return render(request, 'pages/about.html', {"value": "passed value from dictionary"})
+
+def show_time(request):
+    return render(request, 'pages/show_time.html', {"time": datetime_safe.datetime.now()})
